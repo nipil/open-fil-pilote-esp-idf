0365f4705754ee2835fb415e93ea9c3fdedba429
nipil <nipil@users.noreply.github.com>
2022-07-24 21:55:46 +0200

    hardware: supported + current

--- main/web/app.js
+++ main/web/app.js
@@ -1,4 +1,53 @@
+function handleHttpErrors(response) {
+    if (!response.ok) {
+        throw Error(response.status);
+    }
+    return response;
+}
 
+function onload_hardware_types() {
+    fetch('/samples/hardware_types.json')
+        .then(handleHttpErrors)
+        .then(res => res.json())
+        .then(function (json) {
+            // to enable selection of current hardware
+            let current_type_id = json.current_type_id;
+            // do a shallow clone and append 1 argument
+            let sub_template_short = { '<>': 'option', 'value': '${id}', 'html': '${id}: ${desc}' };
+            let sub_template_full = { ...sub_template_short };
+            sub_template_full.selected = true;
+            // special template because of optional argument
+            let template = {
+                'html': function () {
+                    return json2html.render(this, this.id == current_type_id ? sub_template_full : sub_template_short);
+                }
+            };
+            let element = document.getElementById('hardwareTypeSelect');
+            element.innerHTML = json2html.render(json.supported_types, template);
+        })
+        .catch(function (err) {
+            let el = document.getElementById('collapseHardware');
+            el.innerHTML = `Impossible de r&eacute;cup&eacute;rer les types de mat&eacute;riel support&eacute;s (${err})`
+            el.classList.add("bg-danger");
+            el.classList.add("p-3");
+        });
+}
+
+function onload_hardware() {
+    // console.log("onload_hardware");
+    onload_hardware_types();
+    // onload_hardware_parameters();
+}
+
+function onload_body() {
+    console.log("onload_body");
+    onload_hardware();
+    // onload_accounts();
+    // onload_planning();
+    // onload_zones();
+}
+
+/*
 function onload_zones_override() {
     console.log("onload_zone_override");
 }
@@ -35,20 +84,4 @@ function onload_hardware_parameters() {
     console.log("onload_hardware_parameters");
 }
 
-function onload_hardware_types() {
-    console.log("onload_hardware_types");
-}
-
-function onload_hardware() {
-    console.log("onload_hardware");
-    onload_hardware_types();
-    onload_hardware_parameters();
-}
-
-function onload_body() {
-    console.log("onload_body");
-    onload_hardware();
-    onload_accounts();
-    onload_planning();
-    onload_zones();
-}
+*/
