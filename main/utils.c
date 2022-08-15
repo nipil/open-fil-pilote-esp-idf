768368207d2647abe2c9de749196f8299b45c32d
nipil <nipil@users.noreply.github.com>
2022-08-15 16:51:04 +0200

    whitspaces

--- main/utils.c
+++ main/utils.c
@@ -5,15 +5,15 @@
 /* converts time to localtime using timezone */
 void time_to_localtime(time_t *val, struct tm *timeinfo)
 {
-	setenv("TZ", CONFIG_OFP_LOCAL_TIMEZONE_SPEC, 1);
-	tzset();
-	localtime_r(val, timeinfo);
+    setenv("TZ", CONFIG_OFP_LOCAL_TIMEZONE_SPEC, 1);
+    tzset();
+    localtime_r(val, timeinfo);
 }
 
 /* format localtime */
 void localtime_to_string(struct tm *timeinfo, char *buf, int buf_len)
 {
-	strftime(buf, buf_len, "%c %z %Z", timeinfo);
+    strftime(buf, buf_len, "%c %z %Z", timeinfo);
 }
 
 /* display current localtime */
