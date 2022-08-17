480dfc7a1f69b9dd22120f7837b39132eefd9a52
nipil <nipil@users.noreply.github.com>
2022-08-17 13:44:28 +0200

    add useful linux kernel macros

--- main/utils.h
+++ main/utils.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 
+// Some useful macros (source: linux kernel)
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#define sizeof_field(t, f) (sizeof(((t*)0)->f))
+
 void time_to_localtime(time_t *val, struct tm *timeinfo);
 void localtime_to_string(struct tm *timeinfo, char *buf, int buf_len);
 void display_current_localtime(const char *tag);
