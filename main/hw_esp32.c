686fc4363feb1e639f34cb46392d6505662a26ed
nipil <nipil@users.noreply.github.com>
2022-08-27 09:17:43 +0200

    change params to make it generic

--- main/hw_esp32.c
+++ main/hw_esp32.c
@@ -15,14 +15,19 @@ static struct ofp_hw hw_esp32 = {
     .description = "Any ESP32, to target a new hardware or debug software without special hardware",
     .param_count = 2,
     .params = {
-        {.id = "sample_param",
-         .description = "this is a parameter of type string",
-         .type = HW_OFP_PARAM_STRING,
-         .value = {.string_ = "foo"}},
-        {.id = "another_param",
-         .description = "this is a parameter of type integer",
-         .type = HW_OFP_PARAM_INTEGER,
-         .value = {.int_ = 0}}},
+        {
+            .id = "zone_prefix",
+            .description = "this is a parameter of type string",
+            .type = HW_OFP_PARAM_STRING,
+            .value = {.string_ = "z"},
+        },
+        {
+            .id = "zone_count",
+            .description = "Nombre de zones",
+            .type = HW_OFP_PARAM_INTEGER,
+            .value = {.int_ = 0},
+        },
+    },
     .zone_set = {
         .count = 0,
         .zones = NULL,
