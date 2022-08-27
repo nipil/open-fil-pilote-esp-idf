94bb044301e7f8b7efb4ff7dd6b2e5c5f7146d8b
nipil <nipil@users.noreply.github.com>
2022-08-27 10:30:02 +0200

    fix type conflict

--- main/ofp.h
+++ main/ofp.h
@@ -64,7 +64,7 @@ struct ofp_zone_set
 struct ofp_hw; // forward declaration
 typedef bool (*ofp_hw_func)(struct ofp_hw *hw);
 
-struct ofp_hw_func
+struct ofp_hw_methods
 {
     ofp_hw_func init;
     ofp_hw_func apply;
@@ -99,7 +99,7 @@ struct ofp_hw
     // dynamic zone data
     struct ofp_zone_set zone_set;
     // hooks
-    struct ofp_hw_func hw_func;
+    struct ofp_hw_methods hw_func;
     // configurable params
     int param_count;
     struct ofp_hw_param params[];
