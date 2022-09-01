711d6e882aecb14a7dcdfad5352666cdf31ae8ec
nipil <nipil@users.noreply.github.com>
2022-09-01 16:38:31 +0200

    archive un crash pour diag ultérieur

--- main/m_dns.c
+++ main/m_dns.c
@@ -35,6 +35,21 @@ void mdns_start(void)
     // initialize service
     ESP_ERROR_CHECK(mdns_service_add(mdns_instance, "_https", "_tcp", 443, serviceTxtData, 3));
 
+    /*
+     * TODO: investiguer
+     * A crash caught after a wifi reconnection
+     *
+    I (16:35:38.941) m_dns: MDNS hostname: openfilpilote.local
+    ESP_ERROR_CHECK failed: esp_err_t 0x102 (ESP_ERR_INVALID_ARG) at 0x40089370
+    0x40089370: _esp_error_check_failed at C:/esp/esp-idf/components/esp_system/esp_err.c:42
+
+    file: "./main/m_dns.c" line 36
+    func: mdns_start
+    expression: mdns_service_add(mdns_instance, "_https", "_tcp", 443, serviceTxtData, 3)
+
+    abort() was called at PC 0x40089373 on core 0
+    */
+
     // add another TXT item
     ESP_ERROR_CHECK(mdns_service_txt_item_set("_https", "_tcp", "path", "/"));
 }
