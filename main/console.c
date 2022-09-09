a92da2e21fb93606869f33f3c39ed16562e5d1b0
nipil <nipil@users.noreply.github.com>
2022-09-09 06:30:11 +0200

    disable debug

--- main/console.c
+++ main/console.c
@@ -298,7 +298,7 @@ static struct // argument order defined by struct ordering
 static int nvs_clear_cmd(int argc, char **argv)
 {
     printf("\r\n");
-    esp_log_level_set(TAG, ESP_LOG_VERBOSE);
+    // esp_log_level_set(TAG, ESP_LOG_VERBOSE); // DEBUG
     int nerrors = arg_parse(argc, argv, (void **)&nvs_clear_args);
     if (nerrors != 0)
     {
@@ -337,7 +337,7 @@ static struct // argument order defined by struct ordering
 static int nvs_delete_cmd(int argc, char **argv)
 {
     printf("\r\n");
-    esp_log_level_set(TAG, ESP_LOG_VERBOSE);
+    // esp_log_level_set(TAG, ESP_LOG_VERBOSE); // DEBUG
     int nerrors = arg_parse(argc, argv, (void **)&nvs_delete_args);
     if (nerrors != 0)
     {
